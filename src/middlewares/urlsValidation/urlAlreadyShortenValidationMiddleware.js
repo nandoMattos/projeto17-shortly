80fee25b2152d7d915d966b7e212e561cf71bd93
Luiz Fernando Mattos <luizfernandoamattos@gmail.com>
2022-12-20 17:31:50 -0300

    fix: cannot POST and existent URL

--- src/middlewares/urlsValidation/urlAlreadyShortenValidationMiddleware.js
+++ src/middlewares/urlsValidation/urlAlreadyShortenValidationMiddleware.js
@@ -1,3 +1,23 @@
-export default function urlAlreadyShortenValidationMiddleware(req, res, next) {
-  console.log(res.locals.urlInfo);
+import connection from "../../database/db.js";
+
+export default async function urlAlreadyShortenValidationMiddleware(
+  req,
+  res,
+  next
+) {
+  const urlExists = await connection.query(
+    `
+    SELECT id
+    FROM urls
+    WHERE "originalUrl" = $1;
+  `,
+    [req.body.url]
+  );
+
+  if (urlExists.rows[0]) {
+    res.status(409).send("URL já cadastrada.");
+    return;
+  }
+
+  next();
 }
